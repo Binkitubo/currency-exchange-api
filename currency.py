111f318e855f6f9ad84db98a4c523d96c32b8357
Binkitubo <davidlolleria@hotmail.com>
2023-08-11 18:31:40 +0200

    Added function to convert currencies

--- currency.py
+++ currency.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# Sample currency rates
 currency_rates = [
     {"from": "EUR", "to": "USD", "rate": "1.359"},
     {"from": "CAD", "to": "EUR", "rate": "0.732"},
@@ -9,7 +10,7 @@ currency_rates = [
     {"from": "EUR", "to": "CAD", "rate": "1.366"}
 ]
 
-# Example transactions
+# Sample transactions
 transactions = [
     {"sku": "T2006", "amount": "10.00", "currency": "USD"},
     {"sku": "M2007", "amount": "34.57", "currency": "CAD"},
@@ -17,6 +18,18 @@ transactions = [
     {"sku": "T2006", "amount": "7.63", "currency": "EUR"},
     {"sku": "B2009", "amount": "21.23", "currency": "USD"}
 ]
+
+# Helper function to convert amount from one currency to another
+def convert_currency(amount, from_currency, to_currency):
+    rate = 1.0
+
+    if from_currency != to_currency:
+        for cr in currency_rates:
+            if cr['from'] == from_currency and cr['to'] == to_currency:
+                rate = float(cr['rate'])
+                break
+    
+    return round(float(amount) * rate, 2)
 
 @app.route('/currency-rates', methods=['GET']) 
 def get_all_currency_rates(): 
